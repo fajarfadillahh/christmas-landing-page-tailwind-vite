3944038bb25ea8928880ef791da22e221a2886ee
fajarfadillahh <fajaragustian979@gmail.com>
2022-09-22 11:52:44 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Responsive Christmas Website ⛄️
+
+## [Watch it on youtube](https://youtu.be/HrZSQpxfIxw)
+
+### Responsive Christmas Website ⛄️
+
+- Responsive Christmas Website Using HTML CSS & JavaScript
+- Contains animations when scrolling.
+- Smooth scrolling in each section.
+- Includes a dark and light mode.
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+
+💙 This website was created following the tutorial on the channel [Bedimcode](https://www.youtube.com/c/Bedimcode). Let's on subscribe, like, and share with your friends.
+
+![preview img](./src/img/preview.png)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,42 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,sass}"],
   theme: {
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "767px",
+      lg: "968px",
+      xl: "1024px",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#282525",
+      grey: {
+        500: "#5d5656",
+        600: "#918888",
+      },
+      red: {
+        500: "#dc3845",
+        600: "#d42b39",
+      },
+    },
+    fontFamily: {
+      body: ["Poppins", "sans-serif"],
+    },
+    fontWeight: {
+      reguler: 400,
+      medium: 500,
+      semibold: 600,
+    },
+    lineHeight: {
+      title: "140%",
+      text: "180%",
+    },
+    transitionDuration: {
+      400: "400ms",
+    },
     extend: {},
   },
   plugins: [],
